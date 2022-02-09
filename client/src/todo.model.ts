8afccb92ac78e96b224bb2a6c287f2e01bf0367c
Kyle Danny <codingkyle92@gmail.com>
2022-02-09 16:14:38 +0100

    installed axios

--- client/src/todo.model.ts
+++ client/src/todo.model.ts
@@ -2,8 +2,3 @@ export interface Todo {
   id: string;
   text: string;
 }
-
-// export interface ToDoListProps {
-//   items: {id: string, text: string}[];
-//   todoDeleteHandler: (id: string) => void;
-// }
