654daa477eedd5a678be6859cc84794d53f9e9d4
Kyle Danny <codingkyle92@gmail.com>
2022-02-09 16:02:03 +0100

    removed dist

--- server/dist/models/todos.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Todo {
-    constructor(text, _id) {
-        this.text = text;
-        this._id = _id;
-    }
-}
-exports.default = Todo;
