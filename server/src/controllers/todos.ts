d598747d8bbd449d3adff9ccd0b1e1fac5fffec4
Kyle Danny <codingkyle92@gmail.com>
2022-02-10 13:38:30 +0100

    modified post route

--- server/src/controllers/todos.ts
+++ server/src/controllers/todos.ts
@@ -13,7 +13,9 @@ export const createTodo: RequestHandler = async (req, res, next) => {
   const text = req.body.enteredText.text;
   const newTodo = new Todo(text);
   await collections.list?.insertOne(newTodo);
-  res.status(201).json({ message: 'Created the todo', createdTodo: newTodo });
+  const todos = (await collections.list?.find({}).toArray()) as unknown as Todo[];
+
+  res.status(201).json({ message: 'Created the todo', updatedTodos: todos });
 }
 
 export const updateTodo: RequestHandler<{id: string}> = async (req, res, next) => {
@@ -21,7 +23,6 @@ export const updateTodo: RequestHandler<{id: string}> = async (req, res, next) =
   const updatedText: Todo = req.body as Todo;
   const query = { _id: new ObjectId(id) };
   await collections.list!.updateOne(query, { $set: updatedText });
-
   const todos = (await collections.list?.find({}).toArray()) as unknown as Todo[];
   
   res.status(200).json({ message: 'Updated!', updatedTodos: todos });
@@ -31,7 +32,6 @@ export const deleteTodo: RequestHandler = async (req, res, next) => {
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
   const result = await collections.list?.deleteOne(query);
-
   const todos = (await collections.list?.find({}).toArray()) as unknown as Todo[];
 
   res.status(202).json({ message: `Successfully removed game with id ${id}`, updatedTodos: todos});
