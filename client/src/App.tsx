08b10b525902ed726c0b1f73305091958ac2295b
Kyle Danny <codingkyle92@gmail.com>
2022-02-11 11:41:50 +0100

    added routing and amended components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import ToDoList from './components/ToDoList';
-import NewToDoForm from './components/NewToDoForm';
-import { Todo } from './todo.model'
-const axios = require('axios');
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const postToServer = (todos: Todo[]) => {
-    axios.post('http://localhost:3001/todos/', {
-      todos
-    })
-    .then(function (response: object) {
-      console.log(response);
-    })
-  }
-
-  const todoAddHandler = (newTodo: Todo) => {
-    setTodos(prevTodos => [...prevTodos, newTodo]);
-  }
-
-  const todoDeleteHandler = (todoId: string) => {
-    setTodos(prevTodos => {
-      return prevTodos.filter(todo => todo.id !== todoId);
-    });
-  }
-
-  useEffect(() => {
-    postToServer(todos);
-  }, [todos])
 
   return (
     <div className="App">
-      <NewToDoForm todoAddHandler={todoAddHandler} />
-      <ToDoList items={todos} todoDeleteHandler={todoDeleteHandler} />
+      <Routes> 
+        <Route path="/dashboard" element={<ToDoList />} />
+      </Routes>
     </div>
   );
 }
--- client/src/components/ToDoList.tsx
+++ client/src/components/ToDoList.tsx
@@ -1,19 +1,51 @@
+import React, { useEffect, useState } from 'react';
 import './ToDoList.css'
+import NewToDoForm from './NewToDoForm';
+import { Todo } from '../todo.model'
+const axios = require('axios');
 
-interface ToDoListProps {
-  items: {id: string, text: string}[];
-  todoDeleteHandler: (id: string) => void;
-}
+const ToDoList: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-const ToDoList: React.FC<ToDoListProps> = ({items, todoDeleteHandler }) => {
+  const postToServer = (todos: Todo[]) => {
+    axios.post('http://localhost:3001/todos/', {
+      todos
+    })
+    .then(function (response: object) {
+      console.log(response);
+    })
+  }
 
-  return <ul>
-    {items.map((todo) => 
-      <li key={todo.id}>
-        <span>{todo.text}</span>
-        <button onClick={todoDeleteHandler.bind(null, todo.id)}>DELETE</button>
-      </li>)}
-  </ul>
+  const todoAddHandler = (newTodo: Todo) => {
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+  }
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  }
+
+  useEffect(() => {
+    postToServer(todos);
+  }, [todos])
+
+  return (
+    <div>
+      <div>
+        <h1> Todo List </h1>
+        <h2> Dashboard </h2>
+        <NewToDoForm todoAddHandler={todoAddHandler} />
+      </div>
+      <ul>
+        {todos.map((todo) => 
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={todoDeleteHandler.bind(null, todo.id)}>DELETE</button>
+          </li>)}
+      </ul>
+    </div>
+  )
 } 
 
 export default ToDoList;
